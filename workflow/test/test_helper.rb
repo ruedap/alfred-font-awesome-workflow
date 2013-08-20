076452ffabb60da0fdbb039143ba4dc027fc61f2
ruedap <ruedap@ruedap.com>
2013-08-21 04:44:53 +0900

    Implement test for Travis CI

---
+++ workflow/test/test_helper.rb
@@ -0,0 +1,7 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname('..')))
+
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require 'bundle/bundler/setup'
+require 'alfred'
+
+require 'minitest/autorun'
