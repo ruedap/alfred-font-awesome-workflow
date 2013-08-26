31a04bccbd7e099157794130546a88d6d3146f6e
ruedap <ruedap@ruedap.com>
2013-08-26 15:45:35 +0900

    Implement test for FontAwesome.select!

--- workflow/test/font_awesome_test.rb
+++ workflow/test/font_awesome_test.rb
@@ -10,9 +10,7 @@ describe FontAwesome do
     after  { @icons = nil }
 
     it { @icons.count.must_equal 378 }
-
     it { @icons.first.must_equal 'adjust' }
-
     it { @icons.last.must_equal 'zoom-out' }
 
     it 'includes these strings' do
@@ -23,6 +21,66 @@ describe FontAwesome do
     it 'does not includes these strings' do
       strings = %w(icon)
       strings.each { |str| @icons.wont_include str }
+    end
+  end
+
+  describe '.select!' do
+    before { @icons = FontAwesome.icons }
+    after  { @icons = nil }
+
+    describe 'with `apple`' do
+      before do
+        @queries = %w(apple)
+        FontAwesome.select!(@icons, @queries)
+      end
+      after { @queries = nil }
+
+      it { @icons.must_equal @queries }
+      it { @icons.count.must_equal 1 }
+    end
+
+    describe 'with `left arr`' do
+      before do
+        @queries = %w(left arr)
+        FontAwesome.select!(@icons, @queries)
+      end
+      after { @queries = nil }
+
+      it { @icons.must_equal %w(arrow-left circle-arrow-left long-arrow-left) }
+      it { @icons.count.must_equal 3 }
+    end
+
+    describe 'with `arr left` (reverse)' do
+      before do
+        @queries = %w(arr left)
+        FontAwesome.select!(@icons, @queries)
+      end
+      after { @queries = nil }
+
+      it { @icons.must_equal %w(arrow-left circle-arrow-left long-arrow-left) }
+      it { @icons.count.must_equal 3 }
+    end
+
+    describe 'with `ruedap` (does not match)' do
+      before do
+        @queries = %w(ruedap)
+        FontAwesome.select!(@icons, @queries)
+      end
+      after { @queries = nil }
+
+      it { @icons.must_equal %w() }
+      it { @icons.must_be_empty }
+    end
+
+    describe 'with unknown arguments' do
+      before do
+        @queries = %w()
+        FontAwesome.select!(@icons, @queries)
+      end
+      after { @queries = nil }
+
+      it { @icons.must_equal FontAwesome.icons }
+      it { @icons.count.must_equal 378 }
     end
   end
 end
