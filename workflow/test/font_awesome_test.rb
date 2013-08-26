25c8ae6d9c9ca580adf127738bd374560b91f23a
ruedap <ruedap@ruedap.com>
2013-08-26 15:04:25 +0900

    Refactor

--- workflow/test/font_awesome_test.rb
+++ workflow/test/font_awesome_test.rb
@@ -2,38 +2,27 @@ require File.expand_path('test_helper', File.dirname(__FILE__))
 
 describe FontAwesome do
   it 'does not cause an error' do
-    assert_equal false, require('bundle/bundler/setup')
+    require('bundle/bundler/setup').must_equal false
   end
 
   describe '.icons' do
-    before do
-      @icons = FontAwesome.icons
-    end
+    before { @icons = FontAwesome.icons }
+    after  { @icons = nil }
 
-    it 'is 378 pieces' do
-      @icons.count.must_equal 378
-    end
+    it { @icons.count.must_equal 378 }
+
+    it { @icons.first.must_equal 'adjust' }
+
+    it { @icons.last.must_equal 'zoom-out' }
 
     it 'includes these strings' do
       strings = %w(user repeat copy code-fork)
-      strings.each do |str|
-        @icons.include?(str).must_equal true
-      end
+      strings.each { |str| @icons.must_include str }
     end
 
     it 'does not includes these strings' do
       strings = %w(icon)
-      strings.each do |str|
-        @icons.include?(str).must_equal false
-      end
-    end
-
-    it 'is "adjust" is the first element' do
-      @icons.first.must_equal 'adjust'
-    end
-
-    it 'is "zoom-out" is the last element' do
-      @icons.last.must_equal 'zoom-out'
+      strings.each { |str| @icons.wont_include str }
     end
   end
 end
