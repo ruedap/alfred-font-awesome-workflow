750915b14c87659ffc99384057fee2365d63de6f
ruedap <ruedap@ruedap.com>
2014-11-06 21:53:01 +0900

    Remove unnecessary method

--- workflow/main.rb
+++ workflow/main.rb
@@ -4,22 +4,13 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
 
 require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require 'logger'
 require 'bundle/bundler/setup'
 require 'lib/font_awesome'
 
 # Main class
 class Main
-  def initialize(queries, debug = false)
-    xml = FontAwesome.new(queries).to_alfred
-    logging(xml) if debug
-  end
-
-  def logging(string)
-    log_file = File.expand_path('~/Library/Logs/font-awesome-workflow.log')
-    logger = Logger.new(log_file, 'daily')
-    logger.progname = 'Font Awesome Workflow'
-    logger.debug(string)
+  def initialize(queries)
+    FontAwesome.new(queries).to_alfred
   end
 end
 
