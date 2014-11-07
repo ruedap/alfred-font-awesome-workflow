65563f23cba5a2dad9fbbe31d0c743da0a0ad7dc
ruedap <ruedap@ruedap.com>
2014-11-07 23:06:27 +0900

    Introduce require helper

--- workflow/main.rb
+++ workflow/main.rb
@@ -1,18 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+require './require_helper'
 
-require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
-require 'bundle/bundler/setup'
-require 'lib/font_awesome'
-
-# Main class
-class Main
-  def initialize(queries)
-    FontAwesome.new(queries).to_alfred
-  end
-end
-
-# entry point
-Main.new(ARGV)
+FontAwesome.new(ARGV).to_alfred # entry point
---
+++ workflow/require_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+
+require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
+require 'bundle/bundler/setup'
+require 'lib/font_awesome'
