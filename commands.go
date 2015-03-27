9701867a8aaecb86561023c22dadcaf312b613ea
ruedap <ruedap@ruedap.com>
2015-03-28 03:06:05 +0900

    Implement `name` flag in convert command

--- commands.go
+++ commands.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
 )
 
 var Commands = []cli.Command{
 	commandSearch,
+	commandConvert,
 }
 
 var commandSearch = cli.Command{
 	Name:   "search",
 	Usage:  "Search through Font Awesome icons",
 	Action: doSearch,
+}
+
+var commandConvert = cli.Command{
+	Name:   "convert",
+	Usage:  "Convert icon name to each format",
+	Action: doConvert,
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "name", Usage: "Convert to CSS class name"},
+	},
 }
 
 func doSearch(c *cli.Context) {
@@ -38,4 +49,12 @@ func doSearch(c *cli.Context) {
 	}
 
 	r.Print()
+}
+
+func doConvert(c *cli.Context) {
+	name := c.String("name")
+
+	if name != "" {
+		fmt.Print("fa-" + name)
+	}
 }
