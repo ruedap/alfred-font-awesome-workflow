5862a97eb40387d637ef70112785a04ac6fd8009
ruedap <ruedap@ruedap.com>
2015-03-29 01:16:52 +0900

    Add test: TestIconsFind_firstID

--- icons_test.go
+++ icons_test.go
@@ -11,3 +11,13 @@ func TestIconsFind_totalCount(t *testing.T) {
 		t.Errorf("failed to find icon: expected %v, got %v", ex, ac)
 	}
 }
+
+func TestIconsFind_firstID(t *testing.T) {
+	ics := NewIcons()
+
+	ex := "adjust"
+	ac := ics.Find([]string{""})[0].Id
+	if ex != ac {
+		t.Errorf("failed to find icon: expected %v, got %v", ex, ac)
+	}
+}
