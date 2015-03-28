d9982459a6b9bf3da20f7a3f0e277a40a911a3ad
ruedap <ruedap@ruedap.com>
2015-03-29 02:20:16 +0900

    Refactor TestIconsFind_countAll

--- icons_test.go
+++ icons_test.go
@@ -2,35 +2,36 @@ package main
 
 import "testing"
 
-func TestIconsFind_totalCount(t *testing.T) {
-	ics := NewIcons()
-	ai := ics.Find([]string{""})
+func TestIconsFind_countAll(t *testing.T) {
+	fi := findIcons([]string{""})
 
 	ex := 519
-	ac := len(ai)
+	ac := len(fi)
 	if ex != ac {
 		t.Errorf("failed to find icon: expected %v, got %v", ex, ac)
 	}
 }
 
 func TestIconsFind_firstID(t *testing.T) {
-	ics := NewIcons()
-	ai := ics.Find([]string{""})
+	fi := findIcons([]string{""})
 
 	ex := "adjust"
-	ac := ai[0].ID
+	ac := fi[0].ID
 	if ex != ac {
 		t.Errorf("failed to find icon: expected %v, got %v", ex, ac)
 	}
 }
 
 func TestIconsFind_lastID(t *testing.T) {
-	ics := NewIcons()
-	ai := ics.Find([]string{""})
+	fi := findIcons([]string{""})
 
 	ex := "youtube-square"
-	ac := ai[len(ai)-1].ID
+	ac := fi[len(fi)-1].ID
 	if ex != ac {
 		t.Errorf("failed to find icon: expected %v, got %v", ex, ac)
 	}
+}
+
+func findIcons(terms []string) Icons {
+	return NewIcons().Find(terms)
 }
