edf8c8c38a1cd76ae6b28016f56f9a0a0adc8756
ruedap <ruedap@ruedap.com>
2015-04-03 21:35:11 +0900

    Implement newApp function

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 const version string = "0.1.0"
 
 func main() {
+	newApp().Run(os.Args)
+}
+
+func newApp() *cli.App {
 	app := cli.NewApp()
 	app.Name = "faw"
 	app.Version = version
@@ -16,5 +20,5 @@ func main() {
 	app.Author = "ruedap"
 	app.Email = "ruedap@ruedap.com"
 	app.Commands = commands
-	app.Run(os.Args)
+	return app
 }
