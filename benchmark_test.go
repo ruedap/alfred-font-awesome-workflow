db26fd9acf158b703858ffcaea845b493bbe281a
ruedap <ruedap@ruedap.com>
2015-04-06 02:10:46 +0900

    Add BenchmarkIcons_find function

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,3 +7,13 @@ func BenchmarkIcons_newIcons(b *testing.B) {
 		newIcons()
 	}
 }
+
+func BenchmarkIcons_find(b *testing.B) {
+	ics := newIcons()
+	terms := []string{"a", "b", "c"}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ics.find(terms)
+	}
+}
