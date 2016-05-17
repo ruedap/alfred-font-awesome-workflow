a040430203e40c11e420cb0de64dd61d3d5d0f65
ruedap <ruedap@ruedap.com>
2016-05-18 01:20:33 +0900

    (Font Awesome 4.6.3) Fix tests

--- icons_test.go
+++ icons_test.go
@@ -77,7 +77,7 @@ func TestIcons_find_AllIcons(t *testing.T) {
 	fi := testIconsFindHelper([]string{""})
 
 	actual := len(fi)
-	expected := 628
+	expected := 634
 	if actual != expected {
 		t.Errorf("expected %v to eq %v", actual, expected)
 	}
