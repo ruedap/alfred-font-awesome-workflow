16b609ecf8d444bd9cfde9f4910e7b7a4390d4af
ruedap <ruedap@ruedap.com>
2020-08-16 00:12:22 +0900

    test(ts): Add debug code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log('font-awesome-workflow')
+const expected =
+`{"items":[{"uid":"pos-s","title":"pos-s { position: static; }","subtitle":"Paste class name: pos-s","arg":"pos-s","icon":{"type":"fileicon","path":"icon.png"}},{"uid":"pos-r","title":"pos-r { position: relative; }","subtitle":"Paste class name: pos-r","arg":"pos-r","icon":{"type":"fileicon","path":"icon.png"}}]}`;
+
+console.log(expected)
