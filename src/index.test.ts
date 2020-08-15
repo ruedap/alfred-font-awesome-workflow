08f19f59ebc645d6fb6d4a98d006c2033b6d68b2
ruedap <ruedap@ruedap.com>
2020-08-16 00:32:35 +0900

    test(ts): Add example test

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { foo } from './index'
+
+test("foo()", () => {
+  expect(foo('yay')).toBe('foo yay')
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+
+export const foo = (arg: string) => `foo ${arg}`
+
 const expected =
 `{"items":[{"uid":"pos-s","title":"pos-s { position: static; }","subtitle":"Paste class name: pos-s","arg":"pos-s","icon":{"type":"fileicon","path":"icon.png"}},{"uid":"pos-r","title":"pos-r { position: relative; }","subtitle":"Paste class name: pos-r","arg":"pos-r","icon":{"type":"fileicon","path":"icon.png"}}]}`;
 
