6936892441cb22535089ab98126d7f2e16c5f8e3
ruedap <ruedap@ruedap.com>
2020-08-16 15:33:56 +0900

    refactor(src): Remove example code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
 export const foo = (arg: string) => `foo ${arg}`;
-
-const expected = `{"items":[{"uid":"pos-s","title":"pos-s { position: static; }","subtitle":"Paste class name: pos-s","arg":"pos-s","icon":{"type":"fileicon","path":"icon.png"}},{"uid":"pos-r","title":"pos-r { position: relative; }","subtitle":"Paste class name: pos-r","arg":"pos-r","icon":{"type":"fileicon","path":"icon.png"}}]}`;
-
-console.log(expected);
