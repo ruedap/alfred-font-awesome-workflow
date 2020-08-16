e5157ce28d0f2a66f8598d9b1d84aeb79fd11bfd
ruedap <ruedap@ruedap.com>
2020-08-16 21:51:45 +0900

    feat(src): Implement getFindArgsString()

--- src/query.ts
+++ src/query.ts
@@ -4,4 +4,10 @@ export const getArgs = (): string[] => {
 };
 
 const findFlag = "--find";
+
 export const isFind = (args: string[]): boolean => args.includes(findFlag);
+
+export const getFindArgsString = (args: string[]): string => {
+  const index = args.findIndex((value) => value === findFlag);
+  return args.slice(index).join(" ");
+};
