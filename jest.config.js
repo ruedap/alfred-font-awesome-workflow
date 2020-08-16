a63598904ccb10cfd589625a07447382964de6df
ruedap <ruedap@ruedap.com>
2020-08-16 15:53:50 +0900

    build(webpack, ts, jest): Add root alias to path resolve settings

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.+)": "<rootDir>/$1",
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,8 @@ module.exports = {
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
   },
 };
