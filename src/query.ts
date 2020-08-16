2a2164c93310183bceab861a070218da8ee9a262
ruedap <ruedap@ruedap.com>
2020-08-16 21:51:14 +0900

    feat(src): Implement isFind()

--- src/query.ts
+++ src/query.ts
@@ -2,3 +2,6 @@ export const getArgs = (): string[] => {
   const args = process.argv.slice(2);
   return args;
 };
+
+const findFlag = "--find";
+export const isFind = (args: string[]): boolean => args.includes(findFlag);
