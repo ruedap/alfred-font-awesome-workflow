6c1ee58a7d5e713d9910cea2ac006ca0e1284559
ruedap <ruedap@ruedap.com>
2020-08-16 22:36:44 +0900

    test(src): Add test for toResponseItem()

--- src/icons.test.ts
+++ src/icons.test.ts
@@ -1,4 +1,25 @@
-import { getAllIcons } from "./icons";
+import { toResponseItem, getAllIcons } from "./icons";
+
+test("toResponseItem()", () => {
+  const faIconObject = {
+    name: "font-awesome",
+    free: ["brands"],
+    label: "Font Awesome",
+    search: { terms: ["meanpath"] },
+    styles: ["brands"],
+    unicode: "f2b4",
+  };
+
+  const actual = toResponseItem(faIconObject);
+  const expected = {
+    uid: "font-awesome",
+    title: "font-awesome",
+    subtitle: "font-awesome",
+    arg: "f2b4",
+    icon: { path: "./icons/font-awesome.png" },
+  };
+  expect(actual).toStrictEqual(expected);
+});
 
 test("getAllIcons()", () => {
   const allIcons = getAllIcons();
