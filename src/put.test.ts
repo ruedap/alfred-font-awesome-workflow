67a1c60127c77412a260ca7ca55b35a99cb770fe
ruedap <ruedap@ruedap.com>
2020-08-17 00:56:22 +0900

    test(src): Add tests for putCode()

--- src/put.test.ts
+++ src/put.test.ts
@@ -1,4 +1,4 @@
-import { putName } from "./put";
+import { putName, putCode } from "./put";
 
 describe("putName()", () => {
   test("empty", () => {
@@ -9,5 +9,23 @@ describe("putName()", () => {
   test("font-awesome", () => {
     const actual = putName("font-awesome");
     expect(actual).toBe("fa-font-awesome");
+  });
+});
+
+describe("putCode()", () => {
+  test("empty", () => {
+    const actual = putCode("");
+    expect(actual).toBe(null);
+  });
+
+  test("font-awesome", () => {
+    const actual = putCode("font-awesome");
+    expect(actual).toBe("f2b4");
+  });
+
+  // see: https://github.com/FortAwesome/Font-Awesome/blob/master/UPGRADING.md#512x513x-to-5140
+  test("edge-legacy", () => {
+    const actual = putCode("edge-legacy");
+    expect(actual).toBe("f978");
   });
 });
