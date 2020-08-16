7e9465114c9c7a11c3d332f38a787f245bce3a26
ruedap <ruedap@ruedap.com>
2020-08-16 23:02:38 +0900

    feat(src): Use methods for search

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-import { toJson } from "./alfred/response";
-import { getAllIcons } from "./icons";
+import { getArgs, isFind, getFindArgsString } from "./query";
+import { getAllIconsObject } from "./assets/icons_object";
+import { search, toJson } from "./search";
 
-console.log(toJson(getAllIcons()));
+const main = () => {
+  const args = getArgs();
+  const list = getAllIconsObject();
+  const keys = ["name", "search.terms"];
+
+  if (isFind(args)) {
+    const query = getFindArgsString(args);
+    const searchResult = search(list, keys, query);
+    console.log(toJson(searchResult));
+    return;
+  }
+
+  // no args
+  console.log(toJson(search(list, keys, "")));
+};
+
+main();
