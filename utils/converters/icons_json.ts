762b8b57d24d7fe944ae431080a3807cbe2415a3
ruedap <ruedap@ruedap.com>
2020-09-05 15:58:11 +0900

    chore(utils): Implement icons_json.ts

---
+++ utils/converters/icons_json.ts
@@ -0,0 +1,12 @@
+import fs from "fs";
+
+const INPUT_ICONS_JSON_PATH =  "./font-awesome/metadata/icons.json";
+const OUTPUT_ICONS_JSON_PATH = "./assets/icons/icons.json";
+
+const main = () => {
+  fs.copyFileSync(INPUT_ICONS_JSON_PATH, OUTPUT_ICONS_JSON_PATH);
+  const json = JSON.parse(String(fs.readFileSync(OUTPUT_ICONS_JSON_PATH)))
+  console.log(`Output assets/icons.json for ${json.length} icons`);
+};
+
+main();
