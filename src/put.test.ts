d54080bcabfaa5d4cb9063b165c9f52aed3dfbd5
ruedap <ruedap@ruedap.com>
2020-09-06 12:48:59 +0900

    test(src): Fix tests for put functions

--- src/put.test.ts
+++ src/put.test.ts
@@ -1,62 +1,106 @@
 import { putCode, putName, putRef, putUrl } from "./put";
+import { TQuery } from "./query";
+
+const EXAMPLE_QUERY_EMPTY = JSON.stringify({
+  name: "",
+  style: "solid",
+} as TQuery);
+
+const EXAMPLE_QUERY_FONT_AWESOME = JSON.stringify({
+  name: "font-awesome",
+  style: "brands",
+} as TQuery);
+
+const EXAMPLE_QUERY_EDGE_LEGACY = JSON.stringify({
+  name: "edge-legacy",
+  style: "brands",
+} as TQuery);
+
+const EXAMPLE_QUERY_ANGRY_REGULAR = JSON.stringify({
+  name: "angry",
+  style: "regular",
+} as TQuery);
+
+const EXAMPLE_QUERY_ANGRY_SOLID = JSON.stringify({
+  name: "angry",
+  style: "solid",
+} as TQuery);
 
 describe("putName()", () => {
   test("empty", () => {
-    const actual = putName("");
+    const actual = putName(EXAMPLE_QUERY_EMPTY);
     expect(actual).toBe(null);
   });
 
   test("font-awesome", () => {
-    const actual = putName("font-awesome");
-    expect(actual).toBe("fa-font-awesome");
+    const actual = putName(EXAMPLE_QUERY_FONT_AWESOME);
+    expect(actual).toBe("fab fa-font-awesome");
+  });
+
+  test("angry (regular)", () => {
+    const actual = putName(EXAMPLE_QUERY_ANGRY_REGULAR);
+    expect(actual).toBe("far fa-angry");
+  });
+
+  test("angry (solid)", () => {
+    const actual = putName(EXAMPLE_QUERY_ANGRY_SOLID);
+    expect(actual).toBe("fas fa-angry");
   });
 });
 
 describe("putCode()", () => {
   test("empty", () => {
-    const actual = putCode("");
+    const actual = putCode(EXAMPLE_QUERY_EMPTY);
     expect(actual).toBe(null);
   });
 
   test("font-awesome", () => {
-    const actual = putCode("font-awesome");
+    const actual = putCode(EXAMPLE_QUERY_FONT_AWESOME);
     expect(actual).toBe("f2b4");
   });
 
   // see: https://github.com/FortAwesome/Font-Awesome/blob/master/UPGRADING.md#512x513x-to-5140
   test("box-tissue", () => {
-    const actual = putCode("box-tissue");
+    const query = JSON.stringify({
+      name: "box-tissue",
+      style: "solid",
+    } as TQuery);
+    const actual = putCode(query);
     expect(actual).toBe("e05b");
   });
 
   // see: https://github.com/FortAwesome/Font-Awesome/blob/master/UPGRADING.md#512x513x-to-5140
   test("edge-legacy", () => {
-    const actual = putCode("edge-legacy");
+    const actual = putCode(EXAMPLE_QUERY_EDGE_LEGACY);
     expect(actual).toBe("e078");
   });
 });
 
 describe("putRef()", () => {
   test("empty", () => {
-    const actual = putRef("");
+    const actual = putRef(EXAMPLE_QUERY_EMPTY);
     expect(actual).toBe(null);
   });
 
   test("font-awesome", () => {
-    const actual = putRef("font-awesome");
+    const actual = putRef(EXAMPLE_QUERY_FONT_AWESOME);
     const expected = String.fromCodePoint(parseInt("f2b4", 16));
     expect(actual).toStrictEqual(expected);
   });
 
   // see: https://github.com/FortAwesome/Font-Awesome/blob/master/UPGRADING.md#512x513x-to-5140
   xtest("edge-legacy", () => {
-    const actual = putCode("edge-legacy");
+    const actual = putCode(EXAMPLE_QUERY_EDGE_LEGACY);
     const expected = String.fromCodePoint(parseInt("f978", 16));
     expect(actual).toStrictEqual(expected);
   });
 
   test("ad", () => {
-    const actual = putRef("ad");
+    const query = JSON.stringify({
+      name: "ad",
+      style: "solid",
+    } as TQuery);
+    const actual = putRef(query);
     const expected = String.fromCodePoint(parseInt("f641", 16));
     expect(actual).toStrictEqual(expected);
   });
@@ -64,12 +108,12 @@ describe("putRef()", () => {
 
 describe("putUrl()", () => {
   test("empty", () => {
-    const actual = putUrl("");
+    const actual = putUrl(EXAMPLE_QUERY_EMPTY);
     expect(actual).toBe(null);
   });
 
   test("font-awesome", () => {
-    const actual = putUrl("font-awesome");
+    const actual = putUrl(EXAMPLE_QUERY_FONT_AWESOME);
     expect(actual).toBe("https://fontawesome.com/icons/font-awesome");
   });
 });
